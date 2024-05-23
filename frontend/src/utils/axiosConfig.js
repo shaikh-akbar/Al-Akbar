10aa1b8866cedf16aad932afef6c29f19b42c404
shaikh-akbar <akbar000785@gmail.com>
2024-05-23 20:44:26 +0530

    base url

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,5 +1,5 @@
 
-export const base_url = "http://localhost:5000/api/"
+export const base_url = "https://alakbar-one.vercel.app/api/"
 
 const getTokenFromLocalStorage = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
