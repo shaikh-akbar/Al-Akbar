278287194792a74caf770df99db7b59f460d1ecf
shaikh-akbar <akbar000785@gmail.com>
2024-05-26 18:52:37 +0530

    cod

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
